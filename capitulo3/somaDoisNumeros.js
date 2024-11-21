cdb2e4b929f35a498dbbf9da22b55f86f19f5c0a
Renildo Ribeiro Agostini <renildoribeiroagostini@gmail.com>
2024-11-21 09:53:11 -0300

    Criando o prompt-sync pelo prompt de comando

--- capitulo3/somaDoisNumeros.js
+++ capitulo3/somaDoisNumeros.js
@@ -1,8 +1,12 @@
+//adicionar pacote para entrada de dados
 const prompt = require("prompt-sync")();
 
+//le os números
 const num1 = Number(prompt("1º Numero: "));
 const num2 = Number(prompt("2º Numero: "));
 
+//faz a soma dos números
 const soma = num1 + num2;
 
-console.log(`${soma}`)
+//exibe o resultado
+console.log(`A soma do número ${num1} com o número ${num2} é: ${soma}`)
